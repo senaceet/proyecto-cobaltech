b20cfa864cb30d71becc9b628cd27963ddfb2445
christianleon50 <christian.leon@misena.edu.co>
2020-05-16 15:34:56 -0500

    Update index.php

--- 3. Desarrollo/2. Código fuente/cobaltech/index.php
+++ 3. Desarrollo/2. Código fuente/cobaltech/index.php
@@ -14,8 +14,13 @@
 */
 require_once('models/database/DB.php');
 require_once('models/Ciudad.php');
+require_once('models/Factura.php');
+require_once('models/Garantia.php');
 require_once('models/Marca.php');
+require_once('models/Pago.php');
+require_once('models/Producto.php');
 require_once('models/Proveedor.php');
+require_once('models/Rol.php');
 require_once('models/TarjetaCredito.php');
 
 $controller = isset($_REQUEST['controller']) ? $_REQUEST['controller'] : 'cobaltech';
