eae707673395412efd8b703f24f0de43634aa4e9
christianleon50 <christian.leon@misena.edu.co>
2020-05-17 11:52:03 -0500

    Fix

--- 3. Desarrollo/2. Código fuente/cobaltech/views/categoria/index.php
+++ 3. Desarrollo/2. Código fuente/cobaltech/views/categoria/index.php
@@ -3,26 +3,30 @@
   <section class="section">
     <div class="section-header">
       <h1>Categoría de productos</h1>
+    </div>
+    <div class="section-body">
+      <div class="card">
+        <div class="card-body">
+          <a href="" class="btn btn-info">Agregar categoría</a> <br><br>
+          <table class="table table-sm table-hover table-striped table-bordered">
+            <tr class="bg-success text-white ">
+              <td>ID</td>
+              <td>CATEGORIA</td>
+            </tr>
+            <?php
+              foreach(parent::get_all() as $result){
+            ?>
+            <tr>
+              <td><?php echo $result->Id_categoria ?></td>
+              <td><?php echo $result->Nombre_categoria ?></td>
+            </tr>
+            <?php } ?>
+            <tr>
+              <td colspan="2" class="text-center"><?php echo count(parent::get_all()) ?> Productos registrados</td>
+            </tr>
+          </table>
+        </div>
       </div>
-        <div class="section-body">
-
-        <div class="card">
-           <div class="card-body">
-           <a href="" class="btn btn-info">Agregar categoría</a> <br><br>
-            <table class="table table-sm table-hover table-striped table-bordered">
-                <tr class="bg-success text-white ">
-          <td>ID</td>
-          <td>CATEGORIA</td>
-        </tr>
-        <?php
-          foreach(parent::get_all() as $result){
-        ?>
-        <tr>
-          <td><?php echo $result->Id_categoria ?></td>
-          <td><?php echo $result->Nombre_categoria ?></td>
-        </tr>
-        <?php  } ?>
-      </table>
     </div>
   </section>
 </div>
