b654058dff31e263a6750f820375694a7d5f3337
christianleon50 <christian.leon@misena.edu.co>
2020-05-23 14:58:52 -0500

    Update index.php

--- 3. Desarrollo/2. Código fuente/cobaltech/views/estadogarantia/index.php
+++ 3. Desarrollo/2. Código fuente/cobaltech/views/estadogarantia/index.php
@@ -9,29 +9,30 @@
         <div class="card-body">
           <a href="?controller=Garantias&method=index" class="btn btn-info">Volver a garantías</a><br><br>
           <div class="table-responsive">
-          <table class="table table-sm table-hover table-striped table-bordered">
-            <tr class="bg-success text-white ">
-              <td>ID</td>
-              <td>ESTADO DE GARANTÍA</td>
-              <td>OPCIONES</td>
-            </tr>
-            <?php
-              foreach(parent::get_all() as $result){
-            ?>
-            <tr>
-              <td><?php echo $result->Id_estado_garantia ?></td>
-              <td><?php echo $result->Estado_garantia ?></td>
-              <td class="text-white">
-                <a class="btn btn-info btn-sm">Detalles</a>
-                <a class="btn btn-warning btn-sm">Editar</a>
-                <a class="btn btn-danger btn-sm">Eliminar</a>
-              </td>
-            </tr>
-            <?php } ?>
-            <tr>
-              <td colspan="3"><?php echo count(parent::Get_All())?> Estado de la garantía </td>
-            </tr>
-          </table>
+            <table class="table table-sm table-hover table-striped table-bordered">
+              <tr class="bg-success text-white ">
+                <td>ID</td>
+                <td>ESTADO DE GARANTÍA</td>
+                <td>OPCIONES</td>
+              </tr>
+              <?php
+                foreach(parent::get_all() as $result){
+              ?>
+              <tr>
+                <td><?php echo $result->Id_estado_garantia ?></td>
+                <td><?php echo $result->Estado_garantia ?></td>
+                <td class="text-white">
+                  <a class="btn btn-info btn-sm">Detalles</a>
+                  <a class="btn btn-warning btn-sm">Editar</a>
+                  <a class="btn btn-danger btn-sm">Eliminar</a>
+                </td>
+              </tr>
+              <?php } ?>
+              <tr>
+                <td colspan="3"><?php echo count(parent::Get_All())?> Estado de la garantía </td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     </div>
