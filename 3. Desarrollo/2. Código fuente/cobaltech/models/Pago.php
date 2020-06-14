f800258d91474eea1ab74f1c306b1006006a21cb
christianleon50 <christian.leon@misena.edu.co>
2020-06-14 11:26:44 -0500

    Update Pago.php

--- 3. Desarrollo/2. Código fuente/cobaltech/models/Pago.php
+++ 3. Desarrollo/2. Código fuente/cobaltech/models/Pago.php
@@ -4,23 +4,14 @@ class Pago extends DB{
    
    public function get_all(){
       try {
-         $query=parent::connect()->prepare(
-         "SELECT DISTINCT p.Id_pago,
-                        f.Id_factura,
-                        tc.Id_tarjeta_credito,
-                        td.Id_tarjeta_debito,
-                        e.Id_efectivo
-            FROM pago p
-            INNER JOIN factura f
-            INNER JOIN tarjeta_credito tc
-            INNER JOIN tarjeta_debito td
-            INNER JOIN efectivo e
-            WHERE p.FacturaId_factura=f.Id_factura AND (
-                  p.Tarjeta_creditoId_tarjeta_credito=tc.Id_tarjeta_credito OR
-                  p.Tarjeta_debitoId_tarjeta_debito=td.Id_tarjeta_debito OR
-                  p.EfectivoId_efectivo=e.Id_efectivo
-            )
-            ORDER BY p.Id_pago ASC
+         $query=parent::connect()->prepare
+         (" SELECT Id_pago,FacturaId_factura,Tarjeta_creditoId_tarjeta_credito,Tarjeta_debitoId_tarjeta_debito,EfectivoId_efectivo
+            FROM pago p,factura f,tarjeta_credito tc,tarjeta_debito td,efectivo e
+            WHERE p.FacturaId_factura=f.Id_factura
+            AND (p.Tarjeta_creditoId_tarjeta_credito=tc.Id_tarjeta_credito
+            OR p.Tarjeta_debitoId_tarjeta_debito=td.Id_tarjeta_debito
+            OR p.EfectivoId_efectivo=e.Id_efectivo)
+            GROUP BY p.Id_pago
          ");
          $query->execute();
          return  $query->fetchAll(PDO::FETCH_OBJ);
