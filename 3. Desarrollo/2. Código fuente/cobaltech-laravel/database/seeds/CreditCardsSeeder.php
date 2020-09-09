7af591a25c800fd395b920b28d110e82772ff7bb
christianleon50 <christian.leon@misena.edu.co>
2020-09-08 20:45:11 -0500

    Restore

---
+++ 3. Desarrollo/2. Código fuente/cobaltech-laravel/database/seeds/CreditCardsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CreditCardsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
