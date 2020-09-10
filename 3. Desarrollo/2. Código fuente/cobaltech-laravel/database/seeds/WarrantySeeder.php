1ded17b9adf52817da5a9a28d33f8f700054f1d4
christianleon50 <christian.leon@misena.edu.co>
2020-09-10 12:39:23 -0500

    FIX

--- 3. Desarrollo/2. Código fuente/cobaltech-laravel/database/seeds/WarrantySeeder.php
+++ 3. Desarrollo/2. Código fuente/cobaltech-laravel/database/seeds/WarrantySeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class WarrantiesStatusSeeder extends Seeder
+class WarrantySeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- 3. Desarrollo/2. Código fuente/cobaltech-laravel/database/seeds/WarrantyStatusSeeder.php
+++ 3. Desarrollo/2. Código fuente/cobaltech-laravel/database/seeds/WarrantyStatusSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class WarrantiesSeeder extends Seeder
+class WarrantyStatusSeeder extends Seeder
 {
     /**
      * Run the database seeds.
