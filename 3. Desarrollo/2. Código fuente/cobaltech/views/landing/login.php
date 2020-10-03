11247642caf00453ef2b75622e28ddd3a383621d
AngieClavijoG <48868306+AngieClavijoG@users.noreply.github.com>
2020-10-03 17:46:36 -0500

    Update login.php

--- 3. Desarrollo/2. Código fuente/cobaltech/views/landing/login.php
+++ 3. Desarrollo/2. Código fuente/cobaltech/views/landing/login.php
@@ -25,6 +25,29 @@
                     </form>
                     <!-- <p class="card-text">Content</p> -->
                 </div>
+                <div class="card-body">
+                    <h5 class="card-title">REGISTRO</h5>
+                    <form action="?controller=seguridad&method=permiso" method="post">
+                    <div class="form-group">
+                            <label for="">Nombre</label>
+                            <input id="" class="form-control" type="text" name="Primer_nombre">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Apellido</label>
+                            <input id="" class="form-control" type="text" name="Primer_apellido">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Correo</label>
+                            <input id="" class="form-control" type="text" name="correo_electronico">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Contraseña</label>
+                            <input id="" class="form-control" type="password" name="contrasena">
+                        </div>
+                        <button class="button ml-auto mr-auto" type="submit">Registrarse</button>
+                    </form>
+                    <!-- <p class="card-text">Content</p> -->
+                </div>
             </div>
         </div>
     </div>
