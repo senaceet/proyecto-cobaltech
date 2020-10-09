8a8cdc60c528ed1260028c165dbcfe2e43e6049a
christian.leon <christian.leon@misena.edu.co>
2020-10-08 19:21:35 -0500

    Update header.php

--- 3. Desarrollo/2. Código fuente/cobaltech/views/layout_admin/header.php
+++ 3. Desarrollo/2. Código fuente/cobaltech/views/layout_admin/header.php
@@ -12,6 +12,7 @@
    <!--<link rel="stylesheet" href="assets/css/estilo.css">-->
    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+   
    <!-- Template CSS -->
    <link rel="stylesheet" href="assets/css/style.css">
    <link rel="stylesheet" href="assets/css/components.css">
